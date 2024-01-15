5be033e6538fe1a41c0bc873a72a17ea00b2bbf5
Raad05 <ashabulraad@gmail.com>
2024-01-16 00:37:40 +0600

    updated hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const ALCHEMY_RPC_URL = process.env.ALCHEMY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    polygon_mumbai: {
+      url: ALCHEMY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
