c27b5b85a9bff091a2a2745912c021d622b07243
Raad05 <ashabulraad@gmail.com>
2024-01-16 00:48:57 +0600

    updated tokenURI in smart contract

--- contracts/ILN.sol
+++ contracts/ILN.sol
@@ -14,7 +14,10 @@ contract ILN is ERC721URIStorage {
 
         uint newItemId = tokenId;
         _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, "Hello");
+        _setTokenURI(
+            newItemId,
+            "https://emerald-obvious-albatross-557.mypinata.cloud/ipfs/QmQgSpbHGkzAcYA1HEFmtHSVhJAH3UvEcAA6R4PnqPK64V"
+        );
 
         console.log(
             "The NFT with ID: %s has been minted to %s",
